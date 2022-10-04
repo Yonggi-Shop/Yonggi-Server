2315d0d8ec9415db3bddcc9b9dc0bc9f1050c475
VictoryJu <kob0805@naver.com>
2022-10-04 23:44:30 +0900

    CORS설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(logger);
