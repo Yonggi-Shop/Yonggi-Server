0f4694b79caec5c15255aaf76de5a934ee550ea1
VictoryJu <kob0805@naver.com>
2022-10-04 23:48:26 +0900

    cors설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -25,7 +25,7 @@ import { env } from 'process';
       password: env.DATABASE_PASSWORD,
       database: env.DATABASE_NAME,
       entities: [User],
-      synchronize: true,
+      synchronize: false,
       autoLoadEntities: true,
     }),
     AuthModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,12 @@ import { logger } from './common/middlewares/logger.middleware';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(logger);
