24dc5550726817a6822dae7e02d0f0bb2ff897e2
VictoryJu <kob0805@naver.com>
2022-10-06 23:21:23 +0900

    Swagger 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './common/middlewares/logger.middleware';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,6 +12,14 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('YonggiServer')
+    .setDescription('용기서버 API 문서')
+    .setVersion('1.0')
+    .addTag('yonggi')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(logger);
